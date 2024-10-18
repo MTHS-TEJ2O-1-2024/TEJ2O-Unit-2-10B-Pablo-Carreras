74fff2e5f634763def7064c1803fec5ca05cef62
Pablo-Carreras <pablo.carreras-canton@mths.ca>
2024-10-18 11:09:43 -0400

    made the code

--- main.ts
+++ main.ts
@@ -1,8 +1,85 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Pablo Carreras
+ * Created on: Oct 2024
  * This program ...
 */
 
-basic.showString('Hello, World!')
+let neopixelStrip: neopixel.Strip = null
+let inputLightLevel: number = 0
+
+basic.clearScreen()
+
+//setup 
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+inputLightLevel = input.lightLevel()
+
+input.onButtonPressed(Button.A, function () {
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+    inputLightLevel = input.lightLevel()
+    basic.pause(1000)
+
+    //  no neopixel 
+    if(inputLightLevel <= 51) {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+    }
+
+    // first neopixel 
+    if (inputLightLevel > 52) {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+    }
+
+    // second neopixel 
+    if (inputLightLevel > 104) {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+    }
+
+    // third neopixel 
+    if (inputLightLevel > 156) {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+    }
+
+
+    // fourth neopixel 
+    if (inputLightLevel > 208) {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Purple))
+        neopixelStrip.show()
+    }
+    neopixelStrip.show()
+    basic.pause(3000)
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+})
+
